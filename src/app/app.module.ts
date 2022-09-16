e311e5dc72616ebf06e1e42a21d235478c58814f
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-16 08:26:04 +0530

    Imported the 3rd party and In-house modules into the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './auth/login/login.module';
+import { LogoutModule } from './auth/logout/logout.module';
+import { SignupModule } from './auth/signup/signup.module';
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    NgxUiLoaderModule,
+    LoginModule,
+    SignupModule,
+    LogoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
