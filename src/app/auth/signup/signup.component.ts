abf949f610ac1379acea4a1e50d8c3651230da5c
Ankit Sharma <ankitsharma101299@gmail.com>
2022-09-16 21:57:43 +0530

    Update the signup componenent

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { interval, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,19 +14,18 @@ import { environment } from 'src/environments/environment';
 export class SignupComponent implements OnInit {
   myForm: FormGroup;
   signup_result = null;
-  loading: boolean = false;
   signup_error = null;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, private ngxLoader: NgxUiLoaderService) {
+  constructor(private router: Router, private fb: FormBuilder, private http: HttpClient, private ngxLoader: NgxUiLoaderService) {
   }
 
   ngOnInit(): void {
     this.myForm = new FormGroup({});
     this.myForm.addControl('email', new FormControl('', [Validators.required]));
-    this.myForm.addControl('password', new FormControl('', [Validators.required, Validators.minLength(8)]));
+    this.myForm.addControl('password', new FormControl('', [Validators.required, Validators.minLength(4)]));
     this.myForm.addControl('confirmPassword', new FormControl(
       '', [Validators.compose(
-        [Validators.required, Validators.minLength(8), this.validateAreEqual.bind(this)]
+        [Validators.required, Validators.minLength(4), this.validateAreEqual.bind(this)]
       )]
     ));
   }
@@ -37,32 +38,49 @@ export class SignupComponent implements OnInit {
 
   onSubmit(form: FormGroup) {
     if (form.valid) {
-      
+
+      // Start the ngX Loader
+      this.ngxLoader.start();
 
       // Make a call to the create user API.
       const { email, password, confirmPassword } = form.value;
 
       if (email && password && confirmPassword) {
         const api_url = environment.BACKEND_DOMAIN + '/user/create';
-        this.http.post(api_url, {
+        this.http.post<{ status: string }>(api_url, {
           email, password, confirmPassword
         }, {
           headers: {
             'Content-Type': 'application/json'
           }
         }).subscribe(data => {
-          this.loading = true;
-          this.signup_result = data;
+          this.signup_result = data.status;
+          // Redirect the user to the login route.
+          // Stop the ngX Loader
+          this.ngxLoader.stop();
+          interval(1000).pipe(take(10))
+            .subscribe(data => {
+              this.signup_result = null;
+              this.signup_error = null;
+              this.router.navigate(['/login']);
+            });
         },
           error => {
-            this.loading = false;
-            if(error && error.message){
-              this.signup_error = error.message;
+            if (error?.error?.error) {
+              this.signup_error = error.error.error;
             }
-            console.log(error);
+
+            // Stop the ngX Loader
+            this.ngxLoader.stop();
+            interval(1000).pipe(take(10))
+              .subscribe(data => {
+                this.signup_result = null;
+                this.signup_error = null;
+              });
           });
       }
-      // Redirect the user to the login route.
+
+      // Perform the clean up
     }
   }
 
